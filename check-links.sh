7474ecfbc49c811fc7804aec602631d24b4f2d59
LJ Sinclair <48849797+ljsinclair@users.noreply.github.com>
2022-12-09 15:25:57 +1100

    Update check-links.sh
    clarified requirements

--- check-links.sh
+++ check-links.sh
@@ -6,4 +6,6 @@ echo Now building site...
 bundle exec jekyll build
 echo Now running htmlproofer...
 bundle exec htmlproofer ./_site
-echo NOTE: Everything but external links needs to be fixed
+echo NOTE: Internal links need to be fixed.
+echo I suspect " 'a' tag is missing a reference" is an accessibility check.
+echo External links can be ignored.
